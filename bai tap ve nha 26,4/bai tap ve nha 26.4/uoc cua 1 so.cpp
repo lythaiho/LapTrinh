4b61f6f1726fa8aff0d793a2d247f236a49c1bb2
lythaiho <49936593+lythaiho@users.noreply.github.com>
2019-05-03 14:09:00 +0700

    bai 1 26/4

--- bai tap ve nha 26,4/bai tap ve nha 26.4/uoc cua 1 so.cpp
+++ bai tap ve nha 26,4/bai tap ve nha 26.4/uoc cua 1 so.cpp
@@ -4,21 +4,22 @@ int main(){
 	printf("nhap so a bat ky tu ban phim \n",a);
 	scanf("%d",&a);
 	if(a<0){
-		printf(" so am khong co uoc");}
-	else{
-	if(a==0){
-		printf(" so 0 la boi cua moi so nguyen khac 0");}
-	else{ if(a==1){
-		printf("uoc cua 1 la chinh no va la 1");}
-	else{ if(a>0,a!=1)
-		for(int b=1;b<=a;b=b+1){
-		if(a % b == 0){
-            printf("  Uoc cua a la %d  ",b);
+		printf(" so am khong co uoc");
 		}
-	}
-}
-}
-}
+	else if(a==0){
+		printf(" so 0 la boi cua moi so nguyen khac 0");
+		}
+	else if(a==1){
+		printf("uoc cua 1 la chinh no va la 1");
+		}
+	else{ // a>1
+	printf(" uoc cua so a la:  ");
+		for(int b=1;b<=a;b=b+1){
+			if(a % b == 0){
+            printf("%d,  ",b);
+				}
+			}
+		}
 }
 
 
